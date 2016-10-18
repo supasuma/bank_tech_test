7ad55da5524b0003adb53b51d5d9957b4f510009
supasuma <supasuma@gmail.com>
2016-10-18 12:16:21 +0100

    added tests for deduct withdrawl

--- spec/bank_controller_spec.rb
+++ spec/bank_controller_spec.rb
@@ -26,6 +26,31 @@ context '#add_deposit' do
 end
 
 
+context '#deduct_withdrawl' do
+  before do
+    bank_controller.add_deposit(100)
+    bank_controller.deduct_withdrawl(50)
+  end
+
+  it 'should update balance if sufficient funds' do
+    expect(bank_controller.balance).to eq(50)
+  end
+
+  it 'should create a new transaction if sufficient funds' do
+    @time_now = Time.now
+    allow(Time).to receive(:now).and_return(@time_now)
+    expect(bank_controller.instance_variable_get(:@current_transaction)).to eq(transaction)
+  end
+
+  it 'should add transaction to transactions array' do
+    expect(bank_controller.transactions).to eq([transaction])
+  end
+
+end
+
+
+
+
 
 
 end
